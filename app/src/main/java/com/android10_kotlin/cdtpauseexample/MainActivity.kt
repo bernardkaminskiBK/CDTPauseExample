648a246147c0e30ccc18a8b4a6a8727c2ce63852
bernardkaminskBK <bernardkaminski.bk@gmail.com>
2021-07-09 12:25:53 +0200

    Add cdt progress bar example

---
+++ app/src/main/java/com/android10_kotlin/cdtpauseexample/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.android10_kotlin.cdtpauseexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var restProgress = 0
+    private var isPaused = false
+    private var isStarted = true
+    private var timer: CountDownTimer? = null
+
+    private var resumeFromMillis: Long? = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //  tvTimer.text = (15).toString()
+
+//        llStartTimer.setOnClickListener {
+//            if (isStarted) {
+//                isPaused = true
+//                isStarted = false
+//            } else {
+//                isPaused = false
+//                isStarted = true
+//                timer(15000, 1000)
+//                toast("Started")
+//            }
+//        }
+
+        isPaused = false
+        btnStart.isEnabled = false
+        timer(15000, 1000).start()
+
+        btnStart.setOnClickListener {
+            isPaused = false
+            timer(15000, 1000).start()
+        }
+
+        btnPause.setOnClickListener {
+            isPaused = true
+        }
+
+        btnResume.setOnClickListener {
+            timer(resumeFromMillis!!, 1000).start()
+            isPaused = false
+        }
+
+    }
+
+    private fun timer(millisInFuture: Long, countDownInterval: Long): CountDownTimer {
+        btnStart.isEnabled = false
+        timer = object : CountDownTimer(millisInFuture, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeRemaining = "${millisUntilFinished / 1000}"
+                if (isPaused) {
+                    tvTimer.text = "${tvTimer.text}"
+                    restProgress--
+                    cancel()
+                } else {
+                    restProgress++
+                    resumeFromMillis = millisUntilFinished
+                    progressBar.progress = 15 - restProgress
+                    tvTimer.text = timeRemaining
+                }
+            }
+
+            override fun onFinish() {
+                restProgress = 0
+                progressBar.progress = 15
+                tvTimer.text = 15.toString()
+                btnStart.isEnabled = true
+            }
+        }
+        return timer!!
+    }
+
+    private fun toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+}
